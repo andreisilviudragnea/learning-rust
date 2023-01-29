9cb3b4996e96df20ae4af76e1dee56a4185d5c54
Andrei Silviu Dragnea <andreisilviudragnea@gmail.com>
2023-01-29 13:33:07 +0200

    Add no return variants - wip

--- src/clippy/question_mark.rs
+++ src/clippy/question_mark.rs
@@ -11,6 +11,18 @@ pub fn option_function_first_none_after_fix(input: Option<String>) -> Option<Str
     Some(input.to_ascii_lowercase())
 }
 
+pub fn option_function_first_none_no_return(input: Option<String>) -> Option<String> {
+    match input {
+        None => None,
+        Some(input) => Some(input.to_ascii_lowercase())
+    }
+}
+
+pub fn option_function_first_none_no_return_after_fix(input: Option<String>) -> Option<String> {
+    let input = input?;
+    Some(input.to_ascii_lowercase())
+}
+
 pub fn option_function_first_some(input: Option<String>) -> Option<String> {
     let input = match input {
         Some(input) => input,
@@ -20,6 +32,18 @@ pub fn option_function_first_some(input: Option<String>) -> Option<String> {
 }
 
 pub fn option_function_first_some_after_fix(input: Option<String>) -> Option<String> {
+    let input = input?;
+    Some(input.to_ascii_lowercase())
+}
+
+pub fn option_function_first_some_no_return(input: Option<String>) -> Option<String> {
+    match input {
+        Some(input) => Some(input.to_ascii_lowercase()),
+        None => None,
+    }
+}
+
+pub fn option_function_first_some_no_return_after_fix(input: Option<String>) -> Option<String> {
     let input = input?;
     Some(input.to_ascii_lowercase())
 }
@@ -35,6 +59,18 @@ pub fn option_function_first_some_no_match_components(input: Option<String>) -> 
 pub fn option_function_first_some_no_match_components_after_fix(
     input: Option<String>,
 ) -> Option<String> {
+    let input = input?;
+    Some(input.to_ascii_lowercase())
+}
+
+pub fn option_function_first_some_no_match_components_no_return(input: Option<String>) -> Option<String> {
+    match input {
+        Some(input) => Some(input.to_ascii_lowercase()),
+        none => none,
+    }
+}
+
+pub fn option_function_first_some_no_match_components_no_return_after_fix(input: Option<String>) -> Option<String> {
     let input = input?;
     Some(input.to_ascii_lowercase())
 }
